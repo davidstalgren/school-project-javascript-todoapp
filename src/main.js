4d6bf2790025eb8494e510bf1161e5a0725bc2fd
David Stålgren <115017669+davidstalgren@users.noreply.github.com>
2022-12-14 10:20:37 +0100

    /feat JS added Todays date and week number

--- src/main.js
+++ src/main.js
@@ -8,3 +8,24 @@ import { shuffle } from './utils';
 // I denna fil har vi lagrat vår "data", i detta exempel en ofullständig kortlek
 import exampleCardDeck from './exampleArray';
  */
+
+const headerDate = document.querySelector('#headerDate');
+const headerWeek = document.querySelector('#headerWeek');
+let now = new Date()
+let day = now.getDate();
+let month = now.getMonth() + 1;
+let year = now.getFullYear();
+let week = getWeek(now)
+let today = day + '/' + month + ' - ' + year;
+
+function getWeek(date) {
+    let startDate = new Date(date.getFullYear(), 0, 1);
+    let days = Math.floor((date - startDate) / (24 * 60 * 60 * 1000));
+    let weekNumber = Math.ceil(days / 7);
+    return weekNumber;
+  }
+
+headerDate.innerHTML = today;
+headerWeek.innerHTML = 'Week ' + week;
+
+console.log(week)
